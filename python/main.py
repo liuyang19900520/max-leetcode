b457dfb478d8552f90e8181734cd6a6a8966c9e0
liuyang19900520 <liuyang19900520@hotmail.com>
2021-01-04 23:54:27 +0900

    No.0070-Climbing-Stairs

--- python/main.py
+++ python/main.py
@@ -2,13 +2,13 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-from No0011_Container_With_Most_Water import Solution0011
+from No0070_Climbing_Stairs import Solution
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    a = Solution0011()
-    height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
-    result = a.maxArea(height);
+    a = Solution()
+    n = 4
+    result = a.climbStairs(n);
     print(result)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
