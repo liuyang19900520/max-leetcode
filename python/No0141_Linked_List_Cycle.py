6e581a697b1a4a758d6ca0ed5bc412721f4939a5
liuyang19900520 <liuyang19900520@hotmail.com>
2021-03-06 22:24:09 +0900

    0001

--- python/No0141_Linked_List_Cycle.py
+++ python/No0141_Linked_List_Cycle.py
@@ -76,4 +76,14 @@ class Solution:
 
         return True
 
+    def hasCycle2(self, head: ListNode) -> bool:
+        seen = set()
+
+        while head:
+            if head in seen:
+                return True
+            seen.add(head)
+            head = head.next
+        return False
+
 # leetcode submit region end(Prohibit modification and deletion)
--- python/main.py
+++ python/main.py
@@ -2,14 +2,13 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-from No0015_3_Sum import Solution
+from No0001_Two_Sum import Solution
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     a = Solution()
 
-    result = a.threeSum([-2,0,1,1,2]
-)
+    result = a.twoSum([1,2,3,4],3)
     print(result)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
