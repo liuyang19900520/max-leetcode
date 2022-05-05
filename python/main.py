a2ccf88ecdfbc9ef0205744b560bb39ccae103a7
liuyang19900520 <liuyang19900520@hotmail.com>
2022-05-05 13:11:44 +0900

    No.0027

--- python/main.py
+++ python/main.py
@@ -1,9 +1,9 @@
-from No0283_Move_Zeros import Solution0283
+from No0027_Remove_Element import Solution
 
 # Press the green button in the gutter to run the script.
-a = Solution0283()
-arr = [0,1,0,3,12]
-a.moveZeroes2(arr)
-print(arr)
+s = Solution()
+arr = [3, 2, 2, 3]
+result = s.removeElement(arr, 3)
+print(result)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
