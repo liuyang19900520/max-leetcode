33bd2a568450431d69fcbff4102915920500e81c
liuyang19900520 <liuyang19900520@hotmail.com>
2022-06-13 23:54:51 +0900

    No.0496 stack

--- python/main.py
+++ python/main.py
@@ -1,9 +1,11 @@
-from No0020_Valid_Parentheses import Solution
+from No0496_Next_Greater_Element_I import Solution
 
 # Press the green button in the gutter to run the script.
 s = Solution()
-
-result = s.isValid("(]")
+res = []
+nums1 = [4, 1, 2]
+nums2 = [1, 3, 4, 2]
+result = s.nextGreaterElement(nums1, nums2)
 print(result)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
