2d6d80158a326e289e4ee849a05854ffc686062d
liuyang19900520 <liuyang19900520@hotmail.com>
2022-06-19 22:48:23 +0900

    No.0217 hashtable

--- python/main.py
+++ python/main.py
@@ -1,11 +1,11 @@
-from No0496_Next_Greater_Element_I import Solution
+from No0217_Contains_Duplicate import Solution
 
 # Press the green button in the gutter to run the script.
 s = Solution()
 res = []
-nums1 = [4, 1, 2]
+nums1 = [4, 1, 2,1]
 nums2 = [1, 3, 4, 2]
-result = s.nextGreaterElement(nums1, nums2)
+result = s.containsDuplicate(nums1)
 print(result)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
