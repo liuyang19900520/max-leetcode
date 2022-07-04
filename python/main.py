0ba626650685ecc1a83c45858aadeff6850adea3
liuyang19900520 <liuyang19900520@hotmail.com>
2022-07-04 23:06:19 +0900

    No.0094 inorder traversal binary tree

--- python/main.py
+++ python/main.py
@@ -1,11 +1,11 @@
-from No0144_Tree_Preorder_Traversal import TreeNode
-from No0144_Tree_Preorder_Traversal import Solution
+from No0094_Binary_Tree_Inorder_Traversal import TreeNode
+from No0094_Binary_Tree_Inorder_Traversal import Solution
 
 # Press the green button in the gutter to run the script.
 s = Solution()
 
 root = TreeNode(1, None, TreeNode(2, TreeNode(3, None, None), None))
-result = s.preorderTraversal(root)
+result = s.inorderTraversal(root)
 print(result)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
