8210c676a149046c89774cc2ce1aadc5f10d7f7c
liuyang19900520 <liuyang19900520@hotmail.com>
2022-07-08 23:07:01 +0900

    No.0145 postorder traversal binary tree

--- python/main.py
+++ python/main.py
@@ -2,10 +2,14 @@ from No0094_Binary_Tree_Inorder_Traversal import TreeNode
 from No0094_Binary_Tree_Inorder_Traversal import Solution
 
 # Press the green button in the gutter to run the script.
-s = Solution()
+# s = Solution()
+#
+# root = TreeNode(1, None, TreeNode(2, TreeNode(3, None, None), None))
+# result = s.inorderTraversal(root)
+# print(result)
 
-root = TreeNode(1, None, TreeNode(2, TreeNode(3, None, None), None))
-result = s.inorderTraversal(root)
-print(result)
+
+test =[1,2,3,4,5,6,7,8,9]
+print(test[4:0:-1])
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
